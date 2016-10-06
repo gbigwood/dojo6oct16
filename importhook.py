73edc0c71d8a60ee83cac6e1c5c581c1f03e6df4
Daniel F Moisset <dmoisset@machinalis.com>
2016-10-06 20:24:39 +0100

    File name lookup

--- importhook.py
+++ importhook.py
@@ -1,11 +1,17 @@
-import imp
 import sys
-
+from importlib.util import find_spec
 
 class InfectImporter():
 
+    def __init__(self):
+        self.inside = False
+
     def find_spec(self, name, path=None, target=None):
-        print("find spec", name, path, target)
+        if not self.inside:
+            self.inside = True
+            spec = find_spec(name, path)
+            print("infect ", spec.origin)
+            self.inside = False
         return None
 
 sys.meta_path = [InfectImporter()] + sys.meta_path
